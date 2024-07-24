8b534574ac092729303d4d9bed429e0bb98e3a35
Josh Petrie <josh@joshpetrie.net>
2024-07-23 19:21:26 -0700

    Use vim.system() instead of vim.fn.jobstart().

--- lua/lightswitch/init.lua
+++ lua/lightswitch/init.lua
@@ -15,13 +15,17 @@ local function update(background)
   end
 end
 
+local function dispatch_update(background)
+  vim.schedule_wrap(function() update(background) end)()
+end
+
 local function check()
   vim.fn.jobstart("defaults read -g AppleInterfaceStyle", {
     on_exit = function(_, result, _)
       if result == 1 then
-        update("light")
+        dispatch_update("light")
       else
-        update("dark")
+        dispatch_update("dark")
       end
     end
   })
@@ -38,9 +42,10 @@ function lightswitch.setup(options)
     local interval = lightswitch.options.interval
     if interval > 0 then
       local timer = assert(vim.uv.new_timer(), "failed to create timer; polling disabled")
-      timer:start(interval, interval, vim.schedule_wrap(function() check() end))
+      timer:start(interval, interval, vim.schedule_wrap(check))
     end
   end
 end
 
 return lightswitch
+
